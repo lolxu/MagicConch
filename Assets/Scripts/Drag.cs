fa7538ca7f5ac7c2cbfa41d423778a52bc229567
DrFree666 <f907294801@gmail.com>
2025-05-24 02:21:56 -0700

    Implemented Audio
    Implemented audio files.
    The drag-go-back could be faster to better fit the audio

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -3,15 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Drag : MonoBehaviour
 {
     [SerializeField] private float m_clickRadius = 0.5f;
+
+    public AudioClip pullSoundEffect; // added by free Drag your sound file here in the Inspector
+    public AudioClip goBackSoundEffect; // added by free Drag your sound file here in the Inspector
+    public AudioSource audioSource; // added by free
+    private bool pullSoundPlayed = false; // added by free
 
     private bool m_isGrabbing = false;
     private void Update()
     {
         if (Input.GetMouseButton(0))
         {
+            if (!pullSoundPlayed)
+            {
+                audioSource.PlayOneShot(pullSoundEffect); // only once
+                pullSoundPlayed = true;
+            }
+
             Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.CircleCast(mouseWorldPos, m_clickRadius, Vector2.zero);
             if (hit && hit.collider.CompareTag("Target"))
@@ -31,9 +44,13 @@ public class Drag : MonoBehaviour
         {
             if (m_isGrabbing)
             {
+                audioSource.PlayOneShot(goBackSoundEffect); // added by free
                 m_isGrabbing = false;
                 GameManager.Instance.EndDialogue.Invoke();
             }
+
+            pullSoundPlayed = false; // added by free, reset for the next drag
         }
     }
+
 }
